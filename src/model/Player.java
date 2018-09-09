0110b59e9ca60e97d8147e4279b50b57145eddcf
Christian Andrew C <Christianandrew741@gmail.com>
2018-09-09 12:51:23 -0500

    Player class modified

--- src/model/Player.java
+++ src/model/Player.java
@@ -2,4 +2,54 @@ package model;
 
 public class Player {
 
+
+	//CONSTANTS
+	//ATTRIBUTES
+	private int ranking;
+	private  String nickName;
+	private long ping;
+	
+	//RELATIONS 
+	//BUILD METHOD
+	
+	public Player(String nickName) {
+		ranking = 0;
+		
+		
+	}
+	
+	//GETTER AND SETTER METHODS
+
+	public int getRanking() {
+		return ranking;
+	}
+
+	public void setRanking(int ranking) {
+		this.ranking = ranking;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public long getPing() {
+		return ping;
+	}
+
+	public void setPing(long ping) {
+		this.ping = ping;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
