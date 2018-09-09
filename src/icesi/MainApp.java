5b630e25bee4ea7aad13ac85ade659cfb77ac269
Kliver Daniel <kliver.giron1@correo.icesi.edu.co>
2018-09-09 18:09:39 -0500

    Cretaing folder view with DarkTheme css and in Controller the MainApp class

---
+++ src/icesi/MainApp.java
@@ -0,0 +1,18 @@
+package icesi;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
